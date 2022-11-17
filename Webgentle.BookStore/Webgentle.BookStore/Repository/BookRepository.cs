89e83552ced3ab91ad6830bd92241958c9caf0e3
Ray Ojuka <ray@pathwaysinternational.com>
2022-11-17 21:16:58 +0300

    Worked on GetBooks view

--- Webgentle.BookStore/Webgentle.BookStore/Repository/BookRepository.cs
+++ Webgentle.BookStore/Webgentle.BookStore/Repository/BookRepository.cs
@@ -25,12 +25,12 @@ namespace Webgentle.BookStore.Repository
         {
             return new List<BookModel>()
             {
-                new BookModel() { Id = 1, Title = "MVC", Author = "Nitish", Description = "This is the description for MVC book"},
-                new BookModel() { Id = 2, Title = "Dot Net Core", Author = "Nitish", Description = "This is the description for Dot Net Core book"},
-                new BookModel() { Id = 3, Title = "C#", Author = "Monika", Description = "This is the description for C# book"},
-                new BookModel() { Id = 4, Title = "Java", Author = "Webgentle", Description = "This is the description for Java book"},
-                new BookModel() { Id = 5, Title = "PHP", Author = "Webgentle", Description = "This is the description for PHP book"},
-                new BookModel() { Id = 6, Title = "Azure DevOps", Author = "Monika", Description = "This is the description for Azure DevOps book"},
+                new BookModel() { Id = 1, Title = "MVC", Author = "Nitish", Description = "This is the description for MVC book", Category = "Programming", Language = "English", TotalPages = 134 },
+                new BookModel() { Id = 2, Title = "Dot Net Core", Author = "Nitish", Description = "This is the description for Dot Net Core book", Category = "Framework", Language = "Chinese", TotalPages = 567 },
+                new BookModel() { Id = 3, Title = "C#", Author = "Monika", Description = "This is the description for C# book", Category = "Developer", Language = "Hindi", TotalPages = 897 },
+                new BookModel() { Id = 4, Title = "Java", Author = "Webgentle", Description = "This is the description for Java book", Category = "concept", Language = "English", TotalPages = 564 },
+                new BookModel() { Id = 5, Title = "PHP", Author = "Webgentle", Description = "This is the description for PHP book", Category = "Programming", Language = "English", TotalPages = 100 },
+                new BookModel() { Id = 6, Title = "Azure DevOps", Author = "Monika", Description = "This is the description for Azure DevOps book", Category = "DevOps", Language = "English", TotalPages = 800 },
             };
         }
     }
